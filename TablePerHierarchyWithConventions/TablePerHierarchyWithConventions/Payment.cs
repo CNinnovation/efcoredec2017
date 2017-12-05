7cd3b2bc4ae871f748290a82787df359c632ee15
Christian Nagel <christian@christiannagel.com>
2017-12-05 10:02:27 +0100

    cleanup

--- TablePerHierarchyWithConventions/TablePerHierarchyWithConventions/Payment.cs
+++ TablePerHierarchyWithConventions/TablePerHierarchyWithConventions/Payment.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TablePerHierarchyWithConventions
+﻿namespace TablePerHierarchyWithConventions
 {
     public class Payment
     {
