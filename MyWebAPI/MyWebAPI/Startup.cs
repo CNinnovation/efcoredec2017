12a36dac2c270809919fb0980c82aaf2833db277
Christian Nagel <christian@christiannagel.com>
2017-12-06 15:23:36 +0100

    Web API .NET Core

---
+++ MyWebAPI/MyWebAPI/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MyWebAPI.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace MyWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<BooksContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("BooksConnection")));
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Books Service API",
+                    Version = "v1",
+                    Description = "Sample service for Professional C# 7",
+                    Contact = new Contact { Name = "Christian Nagel", Url = "https://csharp.christiannagel.com" },
+                    License = new License { Name = "MIT License" }
+                });
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Chapter Services");
+            });
+        }
+    }
+}
